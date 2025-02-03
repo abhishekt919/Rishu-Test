a15e62172f4a1da0252c0058d48fed33f86c7dd1
Abhishek Thakur <rishut919@gmail.com>
2025-02-03 14:06:39 +0530

    Adding nodemailer to sand the mail

--- src/controllers/Booking.js
+++ src/controllers/Booking.js
@@ -1,9 +1,11 @@
 const Booking = require("../models/Booking");
 const Event = require("../models/Events");
 const {sendBookingConfirmation} = require("../helpers/helperFunctions")
+const {sendEmail} = require("../helpers/helperFunctions")
 
 exports.bookTickets = async (req, res) => {
-  const { to, userId, eventId, seats } = req.body;
+  const { to, userId, eventId, seats, userEmail } = req.body;
+
   try {
     const event = await Event.findById(eventId);
     if (!event || event.availableSeats < seats) {
@@ -21,7 +23,11 @@ exports.bookTickets = async (req, res) => {
 
     event.availableSeats -= seats;
     await event.save();
+   const subject = 'Booking Confirmation';
+    const text = `Your booking for the event "${event.title}" scheduled for ${event.date} has been confirmed. You have booked ${seats} seats.`;
+    const html = `<p>Your booking for the event <strong>"${event.title}"</strong> scheduled for <strong>${event.date}</strong> has been confirmed.</p><p>You have booked <strong>${seats}</strong> seats.\nThank you!</p>`;
 
+    await sendEmail(userEmail, subject, text, html);
     // Send SMS Confirmation using helper function
     await sendBookingConfirmation(to, event.title, event.date, seats);
 
